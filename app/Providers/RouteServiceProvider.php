a235e8890d8d8eefbe71fd3be3d04e7f51626916
feifei2005 <yangguangli2005@outlook.com>
2026-05-15 00:50:36 +0800

    fix: replace Passport client route middleware to prevent auth:web blocking Bearer tokens

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider
 
         foreach ($router->getRoutes()->getRoutesByName() as $name => $route) {
             if (Str::startsWith($name, ['passport.authorizations', 'passport.tokens', 'passport.clients'])) {
-                $route->middleware(['auth:web,oauth']);
+                $route->action['middleware'] = ['web', 'auth:web,oauth'];
             }
         }
 
